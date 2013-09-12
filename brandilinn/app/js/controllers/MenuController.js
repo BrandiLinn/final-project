9dd7f91e62b7e61fbfeb7244b9708bcdede18ed5
Brandi Johnecheck <brandi.johne@gmail.com>
2013-09-12 01:38:07 -0700

    random stage of floundering

--- brandilinn/app/js/controllers/MenuController.js
+++ brandilinn/app/js/controllers/MenuController.js
@@ -6,4 +6,70 @@ foodMeApp.controller('MenuController',
   $scope.restaurant = Restaurant.get({id: $routeParams.restaurantId});
   $scope.cart = cart;
 
+  //////////////////////
+
+
+var filter = $scope.filter = {
+    price: null,
+    name: null
+  };
+
+  var allRestaurants = Restaurant.query(filterAndSortItems);
+  var allItems = $scope.restaurant;
+  $scope.$watch('filter', filterAndSortItems, true);
+
+  console.log(allItems);
+
+  function filterAndSortItems() {
+    $scope.menuItems = [];
+
+    // filter
+    angular.forEach(allItems, function(item, key) {
+      if (filter.price && filter.price !== item.price) {
+        return;
+      }
+
+      if (filter.name && filter.name !== item.name) {
+        return;
+      }
+
+      $scope.menuItems.push(item);
+    });
+
+
+    // sort
+    $scope.menuItems.sort(function(a, b) {
+      if (a[filter.sortBy] > b[filter.sortBy]) {
+        return filter.sortAsc ? 1 : -1;
+      }
+
+      if (a[filter.sortBy] < b[filter.sortBy]) {
+        return filter.sortAsc ? -1 : 1;
+      }
+
+      return 0;
+    });
+  };
+
+
+  $scope.sortBy = function(key) {
+    if (filter.sortBy === key) {
+      filter.sortAsc = !filter.sortAsc;
+    } else {
+      filter.sortBy = key;
+      filter.sortAsc = true;
+    }
+  };
+
+
+  $scope.sortIconFor = function(key) {
+    if (filter.sortBy !== key) {
+      return '';
+    }
+
+    return filter.sortAsc ? '\u25B2' : '\u25BC';
+  };
+
+
+
 });
