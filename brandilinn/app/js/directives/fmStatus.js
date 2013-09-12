8d56632c7ce8c05125e2998dd92a6213840a615d
Brandi Johnecheck <brandi.johne@gmail.com>
2013-09-12 07:25:55 -0700

    "we are" directive

---
+++ brandilinn/app/js/directives/fmStatus.js
@@ -0,0 +1,25 @@
+'use strict';
+
+foodMeApp.directive('fmStatus', function() {
+  return {
+    restrict: 'E',
+    link: function(scope, element, attrs) {
+      var number = Math.floor(Math.random()*10);
+      var weAre = [
+        'totally awesome',
+        'hungry',
+        'in ur base',
+        'the lords of this realm',
+        'stealing your cookies',
+        'baking you cookies',
+        'right behind you',
+        'moonlighting as Santa\'s elves',
+        'the greatest thing since sliced bread',
+        'whalers on the moon'
+      ];
+      scope.status = weAre[number];
+    },
+    template:
+      '<p>We\'re also {{status}}!</p>'
+  };
+});
