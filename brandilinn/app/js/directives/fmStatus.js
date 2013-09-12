7a1c2f581ee5c2f6e7385d46936fce9ade8fc3dd
Brandi Johnecheck <brandi.johne@gmail.com>
2013-09-12 08:11:41 -0700

    probably terrible, but click handler works right

--- brandilinn/app/js/directives/fmStatus.js
+++ brandilinn/app/js/directives/fmStatus.js
@@ -4,27 +4,32 @@ foodMeApp.directive('fmStatus', function() {
   return {
     restrict: 'E',
     link: function(scope, element, attrs) {
-      var number = Math.floor(Math.random()*10);
-      var weAre = [
-        'totally awesome',
-        'hungry',
-        'in ur base',
-        'the lords of this realm',
-        'stealing your cookies',
-        'baking you cookies',
-        'right behind you',
-        'moonlighting as Santa\'s elves',
-        'the greatest thing since sliced bread',
-        'whalers on the moon'
-      ];
-      scope.status = weAre[number];
-      element[0].onclick = function() {
-        var number = Math.floor(Math.random()*10);
-        scope.status = weAre[number];
-        console.log(scope.status);
-      }
     },
     template:
-      '<p>We\'re also {{status}}!</p>'
+      '<p><a ng-click="click()">We\'re also {{status}}!</a></p>',
+    controller: function($scope, $element) {
+      function updateStatus() {
+        var number = Math.floor(Math.random()*10);
+        var weAre = [
+          'totally awesome',
+          'hungry',
+          'in ur base',
+          'the lords of this realm',
+          'stealing your cookies',
+          'baking you cookies',
+          'right behind you',
+          'moonlighting as Santa\'s elves',
+          'the greatest thing since sliced bread',
+          'whalers on the moon'
+        ];
+        $scope.status = weAre[number];
+      }
+
+      updateStatus();
+      
+      $scope.click = function() {
+        updateStatus();
+      }
+    }
   };
 });
